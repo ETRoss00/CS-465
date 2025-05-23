6b78702fdac73e2b5a9f1577a79d6236288474ce
ETRoss00 <ezross00@gmail.com>
2025-05-23 10:26:45 -0400

    Module 3 Completed Baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,7 +1,10 @@
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 /* GET travel view */
 const travel = (req, res) => {
     console.log('Inside app_server, controllers, travel.js travel function.');
-    res.render('travel', {title: 'Travlr Getwaways' });
+    res.render('travel', {title: 'Travlr Getwaways', trips });
 };
 
 module.exports = {
